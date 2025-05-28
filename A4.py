8f3f201559f8b55e83745b77395dc535db9d604a
smppro <smpproplayers@gmail.com>
2025-05-29 05:18:34 +0530

    Update A4.py

--- A4.py
+++ A4.py
@@ -1,16 +1,8 @@
-def letter_frequency(sentence):
-    frequency_dist = {}
-    for char in sentence:
-        if char.isalpha():
-            char = char.lower()
-
-        if char in frequency_dist:
-            frequency_dist[char] += 1
-        else:
-            frequency_dist[char] = 1
-    return frequency_dist
-
-
-sentence = input("Enter a Sentence")
-result = letter_frequency(sentence)
-print(result)
+input_str = input("Enter the Input string")
+freq_all = {}
+for x in input_str:
+    if x in freq_all:
+        freq_all[x]+= 1
+    else:
+        freq_all[x]=1
+print(f"Frq of char:{str(freq_all)}")
