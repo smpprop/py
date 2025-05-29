7ed6e4f6dab667e63b3c589e0b12fb87f0aa5392
smppro <smpproplayers@gmail.com>
2025-05-29 05:30:01 +0530

    Update A5.py

--- A5.py
+++ A5.py
@@ -1,31 +1,10 @@
-def nearly_equal(str1, str2):
+def nearly_equal(a, b):
+    if(len(a) != len(b)):       
+        return False
     count = 0
-    i = j = 0
-    while i < len(str1) and j < len(str2):
-        if len(str1) != len(str2):
-            return False
-        if str1[i] != str2[j]:
+    for i in range(len(a)):    
+        if a[i] != b[i]:
             count += 1
-            if len(str1) > len(str2):
-                i += 1
-            elif len(str1) == len(str2):
-                pass
-            else:
-                i -= 1
-        if count > 1:
+        if count > 1:             
             return False
-        j += 1
-        i += 1
-    if count < 2:
-        return True
-
-
-str1 = input("Enter first string \n")
-str2 = input("Enter second string \n")
-
-bool = nearly_equal(str1, str2)
-
-if bool:
-    print("Strings are nearly equal")
-else:
-    print("Strings are not nearly equal")
+    return count == 1             
