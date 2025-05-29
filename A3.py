384cac08d788c66b9b9e5ad7abf63de2b7bdb636
smppro <smpproplayers@gmail.com>
2025-05-29 06:26:13 +0530

    Update A3.py

--- A3.py
+++ A3.py
@@ -1,27 +1,15 @@
 t1 = (1,2,5,7,97,24,6,8,10)
 t2 = (11,13,15)
 
-tp1 = t1[:5]
-tp2 = t1[5:]
-
-print("The first half of the tuple is:")
-print(tp1)
-
-print("The second half of the tuple")
-print(tp2)
+print("First half:\n",t1[:5])
+print("First second:\n",t1[5:])
 
 lst = list()
 for x in t1:
-    if x % 2 ==0:
+    if x % 2 == 0:
         lst.append(x)
-
-print("Tuple with even values")
-tp3 = tuple(lst)
-print(tp3)
-print("Tuple Concatation")
-t1=t1+t2
-print(t1)
-
-print("Maximum and minimum values")
-print(max(t1))
-print(min(t1))
+print("Tupple with even values:\n",tuple(lst))
+t1 = t1+t2
+print("Tuple Concatination:\n",t1)
+print("Tuple max:\n",max(t1))
+print("Tuple min:\n",min(t1))
